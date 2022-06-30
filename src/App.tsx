2325267fa0b70db832a22d3ad025bda19cce9b1d
devkuwa202001 <devkuwa202001@gmail.com>
2022-06-30 07:11:52 +0000

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Suspense, } from "react";
+import { Canvas, } from "@react-three/fiber";
+import { useGLTF, Environment, OrbitControls } from "@react-three/drei";
+import { GLTF } from "three/examples/jsm/loaders/GLTFLoader";
 
-function App() {
+function Sphere() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <mesh visible position={[2, 0.5, 0]} rotation={[0, 0, 0]} scale={[0.25, 0.25, 0.25]} castShadow>
+      <sphereGeometry attach="geometry" args={[2, 32, 32]}/>
+      <meshPhysicalMaterial   attach="material"  color="white"  metalness={0.98}  roughness={0.0}
+      /> </mesh>  
+   );
 }
 
+function Model({modelPath = 'https://car-configurator.s3.ap-northeast-1.amazonaws.com/demo/Supra.glb'}) {
+  const gltf = useGLTF(modelPath) as unknown as GLTF;
+  return (
+    <>
+      {
+        gltf &&
+        <primitive object={gltf.scene} />
+      }
+    </>
+    )
+}
+
+function App({ env = 'https://dl.polyhaven.org/file/ph-assets/HDRIs/hdr/1k/quattro_canti_1k.hdr' }) {
+  return (
+   <div className="App">
+      <Canvas  dpr={Math.max(window.devicePixelRatio, 2)} >
+        <OrbitControls />
+        <color attach="background" args={[0x333353]} />
+        <ambientLight />
+        <pointLight position={[10, 10, 10]} />
+        <Suspense fallback={null}>
+          <Environment background={true} files={env} scene={undefined}  />
+          <Model />
+          <Sphere/>
+          <mesh scale={10}>   <sphereGeometry args={[1, 64, 64]} />   </mesh>
+        </Suspense>
+      </Canvas>
+    </div>  );
+}
 export default App;
